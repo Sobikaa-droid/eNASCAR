82ee5e3a0e1c2f2fe8b29fd2b70aa7e1b8aa7120
unknown <olegbrain0@gmail.com>
2024-04-11 03:46:09 +0500

    Improved Races List

--- apps/racers/views.py
+++ apps/racers/views.py
@@ -24,7 +24,7 @@ class RacerAPIViewSet(viewsets.ModelViewSet):
 class RacerListView(generic.ListView):
     model = Racer
     context_object_name = 'racers'
-    paginate_by = 1
+    paginate_by = 15
     template_name = "racers/racers_list.html"
 
     def get_context_data(self, **kwargs):
